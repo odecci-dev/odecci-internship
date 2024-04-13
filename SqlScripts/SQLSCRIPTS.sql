28203303cc97f6c069624732f3d7a52eccab8428
odecci-dev <dev@odecci.com>
2024-04-14 01:50:44 +0800

    1st commit

---
+++ SqlScripts/SQLSCRIPTS.sql
@@ -0,0 +1,21 @@
+Print '--------------------------------------------'
+Print 'Request Ref No :'
+Print 'Author:        :                           '
+Print 'Date           :                           '
+Print 'Description     :                           '
+Print 'Affected Tables:                           '
+Print '--------------------------------------------'
+GO
+--Put your SQL Scripts Here
+Print '---------- B E F O R E I M A G E -----------'
+Print ''
+GO
+--Put your SQL Scripts Here
+Print '---------- A F T E R I M A G E -------------'
+Print ''
+GO
+--Put your SQL Scripts Here
+Print '---------- C O M P L E T E D  --------------'
+Print ''
+GO
+Print '--------------------------------------------'
