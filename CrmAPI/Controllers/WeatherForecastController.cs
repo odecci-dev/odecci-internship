9c4be498c1aad2b14af1c314f505c88c7a5aece1
odecci-dev <dev@odecci.com>
2024-04-14 01:48:33 +0800

    first commit

--- CrmAPI/Controllers/WeatherForecastController.cs
+++ CrmAPI/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ namespace CrmAPI.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
-{
+{//
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
